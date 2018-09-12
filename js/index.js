94952751565b75e650b9a78747c8d17e9270d77c
shivangipande <shivangi1997@gmail.com>
2018-09-12 19:04:57 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,24 @@
+setTimeout(fade_out, 7000);
+
+function fade_out() {
+  $("#mydiv").fadeOut().empty();
+}
+var pathEls = document.querySelectorAll('.anim path');
+for (var i = 0; i < pathEls.length; i++) {
+  var pathEl = pathEls[i];
+  var offset = anime.setDashoffset(pathEl);
+  pathEl.setAttribute('stroke-dashoffset', offset);
+  anime({
+    targets: pathEl,
+    strokeDashoffset: [offset, 0],
+    duration: 4000,
+    delay: 40,
+    loop: true,
+    direction: 'alternate',
+    easing: 'easeInOutSine',
+    autoplay: true
+  });
+}
 $("body").mousemove(function(event) {
   var eye = $(".eye");
   var x = eye.offset().left + eye.width() / 2;
@@ -11,3 +32,5 @@ $("body").mousemove(function(event) {
     transform: "rotate(" + rot + "deg)"
   });
 });
+
+
